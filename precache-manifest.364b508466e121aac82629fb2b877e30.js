c40c12db7431f5e9bdcf4f96163f0115175c4a33
Susmitha5c3 <lakshmisusmitha13@gmail.com>
2020-06-04 15:21:42 +0530

    Updates

---
+++ precache-manifest.364b508466e121aac82629fb2b877e30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "818ff6da7db606064ac6ffa1bc7dcaf1",
+    "url": "/Pathfinder/index.html"
+  },
+  {
+    "revision": "98ad19373eec025af013",
+    "url": "/Pathfinder/static/css/main.3ca1ad08.chunk.css"
+  },
+  {
+    "revision": "92e683e84d82291783e5",
+    "url": "/Pathfinder/static/js/2.d6eedfb4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pathfinder/static/js/2.d6eedfb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98ad19373eec025af013",
+    "url": "/Pathfinder/static/js/main.e7da094c.chunk.js"
+  },
+  {
+    "revision": "965eb9ad97b283baf0a1",
+    "url": "/Pathfinder/static/js/runtime-main.8b5101fa.js"
+  },
+  {
+    "revision": "c3a3fb7bd3c8a4ccb04d1585e154ba3c",
+    "url": "/Pathfinder/static/media/space.c3a3fb7b.jpg"
+  }
+]);
